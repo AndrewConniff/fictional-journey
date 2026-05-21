c89a817c1a0ce919441633a09feaae6a3ccedf57
Andrew Conniff <anconnif@microsoft.com>
2026-05-21 18:19:09 +0000

    Working!

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using fictional_journey.Models;
 
@@ -6,6 +7,13 @@ namespace fictional_journey.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly IWebHostEnvironment _environment;
+
+    public HomeController(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
     public IActionResult Index()
     {
         return View();
@@ -13,7 +21,35 @@ public class HomeController : Controller
 
     public IActionResult Privacy()
     {
-        return View();
+        var dataPath = Path.Combine(_environment.WebRootPath, "data", "sampledata.json");
+
+        if (!System.IO.File.Exists(dataPath))
+        {
+            return View(new List<EmployeeRecord>());
+        }
+
+        var json = System.IO.File.ReadAllText(dataPath);
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        using var document = JsonDocument.Parse(json);
+        var employees = new List<EmployeeRecord>();
+
+        foreach (var item in document.RootElement.EnumerateArray())
+        {
+            employees.Add(new EmployeeRecord
+            {
+                FirstName = item.GetProperty("FirstName").GetString() ?? string.Empty,
+                LastName = item.GetProperty("LastName").GetString() ?? string.Empty,
+                HiringDate = item.GetProperty("HiringDate").GetString() ?? string.Empty,
+                Department = item.GetProperty("Department").GetString() ?? string.Empty,
+                JobTitle = item.GetProperty("Job Title").GetString() ?? string.Empty
+            });
+        }
+
+        return View(employees);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/EmployeeRecord.cs
@@ -0,0 +1,14 @@
+namespace fictional_journey.Models;
+
+public sealed class EmployeeRecord
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string HiringDate { get; set; } = string.Empty;
+
+    public string Department { get; set; } = string.Empty;
+
+    public string JobTitle { get; set; } = string.Empty;
+}

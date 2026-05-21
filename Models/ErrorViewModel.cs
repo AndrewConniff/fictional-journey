4c77b9cc198b2115dea98ccedc120d10cb61bd99
Andrew Conniff <anconnif@microsoft.com>
2026-05-21 18:11:17 +0000

    mtt live demo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace fictional_journey.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
